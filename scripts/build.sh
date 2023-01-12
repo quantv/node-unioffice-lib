cf007b64d471cdbf9c90c37403d0a887122a5694
Quantv <quantv@live.com>
2023-01-12 09:57:33 +0700

    dung zip de publish

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 go build -buildmode=c-shared -ldflags="-s -w" -o "dist/libspreadsheet.so" src/lib.go src/funcs.go
-gzip dist/libspreadsheet.so
+
+rm dist/libspreadsheet.zip
+zip -j dist/libspreadsheet.zip dist/*
 #go build -buildmode=c-shared -o "include/libspreadsheet.so" src-go/lib.go src-go/funcs.go
 #ldflags="-s -w"
